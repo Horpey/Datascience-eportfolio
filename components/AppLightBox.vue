0a0c4ffcedef522ce37dad943e9adeb52e241381
Tha_UI_Guy <adeniran1059@gmail.com>
2025-01-26 02:36:05 +0100

    image update

--- components/AppLightBox.vue
+++ components/AppLightBox.vue
@@ -16,7 +16,7 @@ const imagesArray = computed(() => props.images.map(image => `images/${image.src
 <template>
   <div class="flex flex-col gap-4">
     <div v-for="(image, index) in props.images" :key="image" class="rounded-lg shadow border border-gray-200 overflow-hidden cursor-pointer hover:scale-105 transition-all duration-300" @click="() => showImg(index)">
-      <img :src="`images/${image.src}`" :alt="image.alt" class="w-full border-b border-gray-200">
+      <img :src="image.src" :alt="image.alt" class="w-full border-b border-gray-200">
       <div class="text-sm py-2 px-4">
         {{ image.alt }}
       </div>

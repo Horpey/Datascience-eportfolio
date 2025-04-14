60f9dd7775908a896b559a6a0baa7ccfbe104c94
Tha_UI_Guy <adeniran1059@gmail.com>
2025-04-15 01:18:21 +0200

    Refactor AppFooter component to simplify class bindings for improved readability and maintainability.

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -5,7 +5,7 @@ const year = computed(() => new Date().getFullYear())
 <template>
   <footer class="bg-white">
     <div class="mx-auto max-w-7xl py-12 md:flex md:items-center md:justify-between flex-col md:flex-row">
-      <p class="mt-8 text-xs/6 text-gray-600 md:mt-0 mb-4 md:mb-0">
+      <p class="mt-8 text-gray-600 md:mt-0 mb-4 md:mb-0">
         &copy; {{ year }}. All rights reserved.
       </p>
       <div class="flex md:justify-center justify-start items-center gap-x-6 md:order-2">
@@ -13,7 +13,7 @@ const year = computed(() => new Date().getFullYear())
         <a
           href="https://github.com/Horpey/Datascience-eportfolio"
           target="_blank"
-          class="text-gray-600 text-xs hover:text-gray-900 transition-colors"
+          class="text-gray-600 hover:text-gray-900 transition-colors"
         >
           <span>Github Repository</span>
         </a>
